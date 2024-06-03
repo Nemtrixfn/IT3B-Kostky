00030e6196b992fcb85cbb48e98e35ce69db7807
Jakub Sedláček <Sedlacek.Jakub@stud.vassboskovice.cz>
2024-06-03 09:41:40 +0200

    jjj

---
+++ IT3B-Kostky/Dice.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace IT3B_Kostky
+{
+    public class Dice
+    {
+        private Grid _grid;
+        private int _currentValue;
+
+
+        public Dice(Grid grid)
+        {
+            _grid = grid;
+            InitializeDiceFaces();
+        }
+
+        private void InitializeDiceFaces()
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                Grid faceGrid = new Grid() { Name = $"face{i}", Visibility = Visibility.Collapsed };
+                switch (i)
+                {
+                    case 1:
+                        faceGrid.Children.Add(CreateDot(0.5, 0.5));
+                        break;
+                    case 2:
+                        faceGrid.Children.Add(CreateDot(0.25, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.75));
+                        break;
+                    case 3:
+                        faceGrid.Children.Add(CreateDot(0.25, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.5, 0.5));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.75));
+                        break;
+                    case 4:
+                        faceGrid.Children.Add(CreateDot(0.25, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.25, 0.75));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.75));
+                        break;
+                    case 5:
+                        faceGrid.Children.Add(CreateDot(0.25, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.5, 0.5));
+                        faceGrid.Children.Add(CreateDot(0.25, 0.75));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.75));
+                        break;
+                    case 6:
+                        faceGrid.Children.Add(CreateDot(0.25, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.25));
+                        faceGrid.Children.Add(CreateDot(0.25, 0.5));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.5));
+                        faceGrid.Children.Add(CreateDot(0.25, 0.75));
+                        faceGrid.Children.Add(CreateDot(0.75, 0.75));
+                        break;
+                }
+                _grid.Children.Add(faceGrid);
+            }
+        }
+
+        private Ellipse CreateDot(double horizontalAlignment, double verticalAlignment)
+        {
+            return new Ellipse
+            {
+                Fill = Brushes.Black,
+                Width = 10,
+                Height = 10,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Margin = new Thickness(horizontalAlignment * _grid.Width - 5, verticalAlignment * _grid.Height - 5, 0, 0)
+            };
+        }
+
+        public void Roll(Random random)
+        {
+            _currentValue = random.Next(1, 7);
+            ShowFace(_currentValue);
+        }
+
+        private void ShowFace(int value)
+        {
+            foreach (UIElement child in _grid.Children)
+            {
+                if (child is Grid faceGrid)
+                {
+                    faceGrid.Visibility = faceGrid.Name == $"face{value}" ? Visibility.Visible : Visibility.Collapsed;
+                }
+            }
+        }
+
+    }
+}
--- IT3B-Kostky/MainWindow.xaml.cs
+++ IT3B-Kostky/MainWindow.xaml.cs
@@ -17,50 +17,28 @@ namespace IT3B_Kostky
  public partial class MainWindow : Window
  {
 
-    private Random random;
+        private Random _random;
+        private Dice[] _diceArray;
 
         public MainWindow()
         {
             InitializeComponent();
-            random = new Random();
+            _random = new Random();
+
+            _diceArray = new Dice[6];
+            _diceArray[0] = new Dice(diceGrid1);
+            _diceArray[1] = new Dice(diceGrid2);
+            _diceArray[2] = new Dice(diceGrid3);
+            _diceArray[3] = new Dice(diceGrid4);
+            _diceArray[4] = new Dice(diceGrid5);
+            _diceArray[5] = new Dice(diceGrid6);
         }
 
         private void RollButton_Click(object sender, RoutedEventArgs e)
         {
-            int diceRoll = random.Next(1, 7); // Generate a number between 1 and 6
-            resultText.Text = $"You rolled a {diceRoll}";
-            ShowDiceFace(diceRoll);
-        }
-
-        private void ShowDiceFace(int face)
-        {
-            dice1.Visibility = Visibility.Collapsed;
-            dice2.Visibility = Visibility.Collapsed;
-            dice3.Visibility = Visibility.Collapsed;
-            dice4.Visibility = Visibility.Collapsed;
-            dice5.Visibility = Visibility.Collapsed;
-            dice6.Visibility = Visibility.Collapsed;
-
-            switch (face)
+            foreach (var dice in _diceArray)
             {
-                case 1:
-                    dice1.Visibility = Visibility.Visible;
-                    break;
-                case 2:
-                    dice2.Visibility = Visibility.Visible;
-                    break;
-                case 3:
-                    dice3.Visibility = Visibility.Visible;
-                    break;
-                case 4:
-                    dice4.Visibility = Visibility.Visible;
-                    break;
-                case 5:
-                    dice5.Visibility = Visibility.Visible;
-                    break;
-                case 6:
-                    dice6.Visibility = Visibility.Visible;
-                    break;
+                dice.Roll(_random);
             }
         }
     }
